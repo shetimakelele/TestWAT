3865d58d46233bdcbbddf3ade61680f55a84d957
sheti <sheti.mak@outlook.fr>
2022-09-24 15:16:43 +0200

    Ajoutez des fichiers projet.

---
+++ TestWAT.Core.Data/Models/Personne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestWAT.Core.Data.Models
+{
+    public class Personne
+    {
+        public int PersonnrId { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public DateTime DateDeNaissance { get; set; }
+
+        
+    }
+}
